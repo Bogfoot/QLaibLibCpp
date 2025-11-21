5d1bb9b3bcf93f2e76a2d7f4bb546302fd2824bc
bogfoot <bogfoot94@yahoo.com>
2025-11-21 12:39:55 +0100

    gitignore edits

--- qlaiblib/metrics/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Metrics package."""
-
-from .core import REGISTRY
-from . import visibility  # noqa: F401 - ensure registration
-from . import qber  # noqa: F401 - ensure registration
-from . import chsh  # noqa: F401 - ensure registration
-
-__all__ = ["REGISTRY"]
