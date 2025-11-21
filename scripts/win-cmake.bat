8f360d64437dcaca9f1a9ac17d32fe872ebd997e
bogfoot <bogfoot94@yahoo.com>
2025-11-21 16:03:51 +0100

    Fuck you windows

--- scripts/win-cmake.bat
+++ scripts/win-cmake.bat
@@ -12,7 +12,8 @@ for %%i in ("%REPO_ROOT%\..\vcpkg") do set VCPKG_ROOT=%%~fi
 
 set BUILD_DIR=%REPO_ROOT%\cpp\build
 set TOOLCHAIN=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
-set QT_PREFIX=%VCPKG_ROOT%\installed\x64-windows\share\Qt6
+rem In manifest mode, deps land in vcpkg_installed inside the repo
+set QT_PREFIX=%REPO_ROOT%\vcpkg_installed\x64-windows\share\qt6
 set QT_DIR=%QT_PREFIX%\cmake
 set COINCFINDER_LIB=%REPO_ROOT%\coincfinder\build\coincfinder_core.lib
 set TDCBASE_LIB=%REPO_ROOT%\DLL_64bit\tdcbase.lib
